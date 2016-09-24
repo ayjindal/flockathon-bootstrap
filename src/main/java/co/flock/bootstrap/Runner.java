d6f28654aacedcdeaa4d8e8a3e578b44e9b44ad0
anshuman <anshumanbajpai123@gmail.com>
2016-09-24 11:47:09 +0530

    init dbmanager

--- src/main/java/co/flock/bootstrap/Runner.java
+++ src/main/java/co/flock/bootstrap/Runner.java
@@ -1,8 +1,13 @@
 package co.flock.bootstrap;
 
+import co.flock.bootstrap.database.DbConfig;
+import co.flock.bootstrap.database.DbManager;
 import org.apache.log4j.Logger;
 
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import static spark.Spark.port;
 import static spark.Spark.staticFileLocation;
@@ -10,12 +15,21 @@ import static spark.Spark.staticFileLocation;
 public class Runner
 {
     private static final Logger _logger = Logger.getLogger(Runner.class);
+    private static DbManager _dbManager;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception
+    {
         _logger.debug("Starting..");
+        _dbManager = new DbManager(getDbConfig());
         port(9000);
-        HashMap map = new HashMap();
         staticFileLocation("/public");
-        map.put("resourcePrefix", "");
+    }
+
+    private static DbConfig getDbConfig()
+    {
+        ResourceBundle bundle = ResourceBundle.getBundle("config", Locale.getDefault());
+        return new DbConfig(bundle.getString("db_host"),
+                Integer.parseInt(bundle.getString("db_port")), bundle.getString("db_name"),
+                bundle.getString("db_username"), bundle.getString("db_password"));
     }
 }
