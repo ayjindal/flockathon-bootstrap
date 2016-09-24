b803f6de51340bab5d5e844f7b6ea39328cc36de
anshuman <anshumanbajpai123@gmail.com>
2016-09-24 11:48:57 +0530

    create table

--- src/main/java/co/flock/bootstrap/database/DbManager.java
+++ src/main/java/co/flock/bootstrap/database/DbManager.java
@@ -4,6 +4,7 @@ import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.Dao.CreateOrUpdateStatus;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
 
@@ -25,6 +26,7 @@ public class DbManager
     {
         JdbcPooledConnectionSource connectionSource = new JdbcPooledConnectionSource(
                 dbConfig.getConnectionURL());
+        TableUtils.createTableIfNotExists(connectionSource, Candidate.class);
         connectionSource.setMaxConnectionAgeMillis(Long.MAX_VALUE);
         _candidateDao = DaoManager.createDao(connectionSource, Candidate.class);
     }
