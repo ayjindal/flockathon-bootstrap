dd43648475b1464954648e7b2a2f768ab1a925da
Bharat Singhvi <bharat.s@flock.co>
2016-09-24 14:35:36 +0530

    Add interviewer-view UI

--- src/main/java/co/flock/bootstrap/Runner.java
+++ src/main/java/co/flock/bootstrap/Runner.java
@@ -3,30 +3,31 @@ package co.flock.bootstrap;
 import co.flock.bootstrap.database.DbConfig;
 import co.flock.bootstrap.database.DbManager;
 import org.apache.log4j.Logger;
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import static spark.Spark.port;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
-public class Runner
-{
+public class Runner {
     private static final Logger _logger = Logger.getLogger(Runner.class);
     private static DbManager _dbManager;
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         _logger.debug("Starting..");
         _dbManager = new DbManager(getDbConfig());
         port(9000);
         staticFileLocation("/public");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("resourcePrefix", "");
+        get("/interviewer-view", (req, res) -> new ModelAndView(map, "index.html"),
+                new MustacheTemplateEngine());
     }
 
-    private static DbConfig getDbConfig()
-    {
+    private static DbConfig getDbConfig() {
         ResourceBundle bundle = ResourceBundle.getBundle("config", Locale.getDefault());
         return new DbConfig(bundle.getString("db_host"),
                 Integer.parseInt(bundle.getString("db_port")), bundle.getString("db_name"),
---
+++ src/main/resources/public/js/initfirepad.js
@@ -0,0 +1,37 @@
+function initializeFirepad() {
+
+    var config = {
+        apiKey: "AIzaSyBv54ql_TM_z2DW7Vmf7OtvEyzu98kZa1M",
+        authDomain: "hyre-8a50b.firebaseio.com/",
+        databaseURL: "https://hyre-8a50b.firebaseio.com/"
+    };
+
+    firebase.initializeApp(config);
+    var firepadRef = getInterviewPadReference();
+
+    var editor = ace.edit("codepad");
+    editor.setTheme("ace/theme/tomorrow_night");
+    editor.setValue("");
+    var session = editor.getSession();
+    session.setUseWrapMode(true);
+    session.setUseWorker(false);
+    session.setMode("ace/mode/java");
+
+    var firepad = Firepad.fromACE(firepadRef, editor);
+}
+
+// Helper to get hash from end of interview URL or generate a random one.
+function getInterviewPadReference() {
+    var ref = firebase.database().ref();
+    var hash = window.location.hash.replace(/#/g, '');
+    if (hash) {
+        ref = ref.child(hash);
+    } else {
+        ref = ref.push();
+        window.location = window.location + '#' + ref.key;
+    }
+    if (typeof console !== 'undefined') {
+        console.log('Firebase data: ', ref.toString());
+    }
+    return ref;
+}
