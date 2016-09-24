309288eb538f84352bb9f10a57042f5c67e54a4b
Ayush Jindal <ayush.j@directi.com>
2016-09-24 17:14:28 +0530

    Add input for time

--- src/main/resources/public/js/createCandidate.js
+++ src/main/resources/public/js/createCandidate.js
@@ -5,13 +5,19 @@ $(document).ready(function()
          name = $("#name").val();
          email = $("#email").val();
          cvLink = $("#cv_link").val();
-         console.log("name: " + name);
+         role = $("#role").val();
+         time = getTime($("#time").val());
          event = getFlockEvent();
          console.log("userId: " + event.userId);
          console.log("chat: " + event.chat);
          createCandidate();
          flock.close();
      });
+
+     function getTime(dateString) {
+        var parts = dateString.match(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})/);
+        return Date.UTC(+parts[3], parts[2]-1, +parts[1], +parts[4], +parts[5]);
+     }
 
      function createCandidate() {
           payload =
@@ -20,14 +26,14 @@ $(document).ready(function()
                   "name": name,
                   "email": email,
                   "cv_link": cvLink,
-                  "role": "platform",
+                  "role": role,
                   "creator_id": event.userId,
               },
               "round": {
                   "collab_link": "http://testCollabLink",
                   "interviewer_id": "testinterviewerid",
                   "question_id": "test_ques_id",
-                  "scheduled_time": "147314131"
+                  "scheduled_time": time
               }
           };
           sendAjaxRequest(baseUrl + "create", "post", payload, function (response) {
