426953b18bda312d76c849e45622741795b1c856
Ayush Jindal <ayush.j@directi.com>
2016-09-24 18:05:16 +0530

    Fetch and show questions

--- src/main/resources/public/js/createCandidate.js
+++ src/main/resources/public/js/createCandidate.js
@@ -1,11 +1,14 @@
 $(document).ready(function()
 {
+     var platformQuestions;
+     getQuestions("platform");
      $("#submit").click(function () {
         console.log("submit clicked")
          name = $("#name").val();
          email = $("#email").val();
          cvLink = $("#cv_link").val();
          role = $("#role").val();
+         question = $("#question").val();
          time = getTime($("#time").val());
          event = getFlockEvent();
          console.log("userId: " + event.userId);
@@ -17,6 +20,23 @@ $(document).ready(function()
      function getTime(dateString) {
         var parts = dateString.match(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})/);
         return Date.UTC(+parts[3], parts[2]-1, +parts[1], +parts[4], +parts[5]);
+     }
+
+     function getQuestions(role) {
+          payload =
+          {
+               "role": role,
+               "groupId": "g:123"
+          }
+          sendAjaxRequest(baseUrl + "questions", "get", payload, function (response) {
+              questions = JSON.parse(response)
+              $.each(questions, function (i, question) {
+                  $('#question').append($('<option>', {
+                      value: question.id,
+                      text : question.title + " (" + question.level + ")"
+                  }));
+              });
+          });
      }
 
      function createCandidate() {
@@ -32,8 +52,8 @@ $(document).ready(function()
               "round": {
                   "collab_link": "http://testCollabLink",
                   "interviewer_id": "testinterviewerid",
-                  "question_id": "test_ques_id",
-                  "scheduled_time": time
+                  "question_id": question,
+                  "scheduled_time": JSON.stringify(time)
               }
           };
           sendAjaxRequest(baseUrl + "create", "post", payload, function (response) {
